f15e54fce9e2a3c397a63ebde0328b8969b107e3
sebastiaocosta1 <136897073+sebastiaocosta1@users.noreply.github.com>
2023-08-25 12:21:08 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from listaDeTarefas import *
+
+lista = ListaDeTarefas()
+
+while 1:  	 
+	lista.menu_principal()
+	op = int(input("\nEscolha uma opção: "))
+	if(op == 0):
+		break
+	elif(op == 1):
+		lista.adicionarTarefa(input("Adicione uma descrição: "))
+	elif(op == 2):
+		lista.listarTarefas()
+	elif(op == 3):
+		id = input("Informe o ID: ")
+		lista.removerTarefa(id)
+	else:
+		print("Opção inválida!")
